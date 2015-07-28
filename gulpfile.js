ebb838d7e93b041acfc18d4db1764eb66da4cf06
Oleksandra korel Kalinina <korery@gmail.com>
2015-07-28 13:26:24 +0300

    New: gulpfile bootstraped

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 
 gulp.paths = {
-  src: ['**/*.js', '!node_modules/**/*']
+  src: ['**/*.js', '!node_modules/**/*', '!ws.public/libs/**/*']
 };
 
 require('require-dir')('./gulp');
