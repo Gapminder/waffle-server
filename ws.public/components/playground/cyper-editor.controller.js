a973315d7022d0275f9bf8efd70967595bcd4d21
Oleksandra korel Kalinina <korery@gmail.com>
2015-07-31 15:13:58 +0300

    New: created cyper playground template (frontend) [refs #10]

--- ws.public/components/playground/cyper-editor.controller.js
+++ ws.public/components/playground/cyper-editor.controller.js
@@ -1,4 +1,46 @@
 'use strict';
 
-angular.module('admin.controllers').controller('CyperEditorController', function CyperEditorController($scope) {
-});
+angular.module('admin.controllers').controller('CyperEditorController', ['CyperEditorService', '$scope', function CyperEditorController(CyperEditorService, $scope) {
+  var self = this;
+  self.choosePattern = choosePattern;
+  self.cleanQuery = cleanQuery;
+  self.runQuery = runQuery;
+  self.loadingQuery = false;
+  self.data = {};
+
+  self.patterns = {
+    MATCH: [
+      'MATCH (n:XXX)-[:YYY]->(m:XXX) WHERE n.name=""',
+      'MATCH (n)-->(m)',
+      'MATCH (n {name:""})-->(m)',
+      'MATCH p = (n)-->(m)',
+      'OPTIONAL MATCH (n)-[r]->(m)'
+    ],
+    WHERE: [
+      'WHERE  n.property <> {value}'
+    ]
+  };
+
+  $scope.query = '';
+
+  function choosePattern (pattern) {
+    var cursorPosition = $('#query').prop('selectionEnd');
+    console.log(cursorPosition);
+    console.log($scope.query);
+    var text = $scope.query;
+    $scope.query = [text.substr(0, cursorPosition), pattern, text.substr(cursorPosition)].join('');
+  }
+
+  function cleanQuery () {
+    $scope.query = '';
+  }
+
+  function runQuery () {
+    self.loadingQuery = true;
+
+    CyperEditorService.runQuery({query: $scope.query}, function(data) {
+      self.data = data;
+      self.loadingQuery = false;
+    });
+  }
+}]);
--- ws.public/components/playground/cyper-editor.service.js
+++ ws.public/components/playground/cyper-editor.service.js
@@ -1,3 +1,29 @@
+'use strict';
+
 /**
- * Created by korel on 30.07.15.
+ * Service that will run cyper queries
  */
+angular.module('admin.services')
+  .service('CyperEditorService', ['$resource', function ($resource) {
+    /**
+     * This object will be contain all information of this service
+     * @type {Object}
+     */
+    var service = {
+      runQuery: runQuery
+    };
+
+    var apiResource = $resource('/api/admin/cyper', {}, {
+      runQuery: {method: 'POST'}
+    });
+
+    return service;
+
+    ////////////
+
+    function runQuery(params, cb) {
+      apiResource.runQuery(params, function (resp) {
+        return cb(resp.error, {data: resp.data}, resp);
+      });
+    }
+  }]);
