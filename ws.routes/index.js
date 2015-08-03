251d3f14bd576a9105b317e85826c4910b7ee28b
Oleksandra korel Kalinina <korery@gmail.com>
2015-08-03 19:19:01 +0300

    New: created route for getting imported data [refs #11]

--- ws.routes/index.js
+++ ws.routes/index.js
@@ -3,5 +3,6 @@
 module.exports = function (app, serviceLocator) {
   require('./api/collections')(app, serviceLocator);
   require('./api/cyper')(app, serviceLocator);
+  require('./api/spreadsheet')(app, serviceLocator);
   require('./auth')(app, serviceLocator);
 };
