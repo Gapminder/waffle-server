d6945a8e017aab007749d89ca91e4ac1783ec196
Oleksandra korel Kalinina <korery@gmail.com>
2015-08-04 16:52:06 +0300

    Fix: moved converting handler to the separate service [fix #14]

--- ws.routes/api/import.js
+++ ws.routes/api/import.js
@@ -1,31 +1,9 @@
 'use strict';
 
-var converter = require('json-2-csv');
-var _ = require('lodash');
-
 module.exports = function (serviceLocator) {
   var app = serviceLocator.getApplication();
   var config = app.get('config');
-  var optionsDefault = config.DEFAULT_OPTIONS_CONVERTING_JSON_TO_CSV;
+  var utils = require('./import.service')(config);
 
-  app.post('/api/import/json2csv', convertJson2Csv);
-
-  function convertJson2Csv(req, res) {
-    var json = req.body.data;
-    var options = _.defaultsDeep(req.body.options, optionsDefault);
-
-    if (!_.isArray(json)) {
-      return res.json({error: 'Data is not an array'});
-    }
-
-    converter.json2csv(json, callback, options);
-
-    function callback(error, csv) {
-      if (error) {
-        return res.json({error: error});
-      }
-
-      return res.json({success: true, data: csv});
-    }
-  }
+  app.post('/api/import/json2csv', utils.convertJson2Csv);
 };
