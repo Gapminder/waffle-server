a8ab867c395ee2e6161344b1087342321762b46c
buchslava <vyacheslav.chub@gmail.com>
2015-08-13 18:11:14 +0300

    Updated: PublishersCollectionController prettify

--- ws.public/components/home/publishers/publishers-collection.controller.js
+++ ws.public/components/home/publishers/publishers-collection.controller.js
@@ -1,47 +1,35 @@
 'use strict';
 
-angular.module('admin.controllers').controller('PublishersCollectionController', [
-  '$rootScope', '$state', 'CollectionsService',
-  function PublishersCollectionController($rootScope, $state, CollectionsService) {
-    var self = this;
-    self.pageTitle = $state.current.data.pageTitle;
-    self.pageType = $state.current.data.pageType;
-    self.data = [];
-    self.currentData = [];
-    self.tagline = 'Waffle Server';
-    var skip = 0;
-    var page = 0;
-    self.limit = 10;
-
-    $rootScope.$on('$stateChangeSuccess', function () {
-      self.page = 0;
+angular.module('admin.controllers')
+  .controller('PublishersCollectionController', [
+    '$state', 'CollectionsService',
+    function ($state, CollectionsService) {
+      var self = this;
       self.data = [];
-    });
+      self.currentData = [];
+      self.limit = 10;
 
-    self.paging = {currentPage: 1};
-    self.pageChanged = getData;
+      self.paging = {currentPage: 1};
+      self.pageChanged = getData;
 
-    getData();
+      getData();
 
-    function getData() {
-      page = self.paging.currentPage - 1;
-      var params = {
-        skip: skip + page * self.limit,
-        limit: self.limit,
-        list: '',
-        action: 'publishers'
-      };
-
-      CollectionsService.getData(params, updateList);
-    }
-
-    function updateList(err, data) {
-      if (err) {
-        console.error(err);
-        return;
+      function getData() {
+        CollectionsService.getData({
+          skip: (self.paging.currentPage - 1) * self.limit,
+          limit: self.limit,
+          list: '',
+          action: 'publishers'
+        }, updateList);
       }
-      self.currentData = data.data;
-      self.totalItems = data.totalItems;
+
+      function updateList(err, data) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        self.currentData = data.data;
+        self.totalItems = data.totalItems;
+      }
     }
-  }
-]);
+  ]);
