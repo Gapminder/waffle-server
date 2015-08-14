2318b364bf762c75827f272a74782d0d26a2eb86
Oleksandra korel Kalinina <korery@gmail.com>
2015-08-14 19:11:55 +0300

    Fix: removed unnecessary mainController function in ws-app.module [refs #19]

--- ws.public/components/ws-app.module.js
+++ ws.public/components/ws-app.module.js
@@ -16,29 +16,3 @@ angular.module('admin', [
     'admin.auth'
   ]);
 // Other libraries are loaded dynamically in the config.js file using the library ocLazyLoad
-
-// todo: remove?
-function mainController($scope, $http) {
-  $scope.formData = {};
-
-  $http.get('/api/collections/list')
-    .success(function(data) {
-      $scope.collections = data;
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
-
-  $scope.getData = function() {
-    $http.get('/api/collections', $scope.formData)
-      .success(function(data) {
-        $scope.formData = {};
-        $scope.data = data;
-        console.log(data);
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
-  };
-}
