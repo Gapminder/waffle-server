c9d5ccd18410ac57a42d5f34ca9986b984580dbd
Dmitriy Shekhovtsov <valorkin@gmail.com>
2015-09-09 17:33:59 +0300

    Preview any table format

--- ws.public/components/file-management/file-list/file-list.controller.js
+++ ws.public/components/file-management/file-list/file-list.controller.js
@@ -78,7 +78,6 @@ module.exports = function (app) {
                 self.tableItems = rows;
                 if (!$scope.$$phase) {
                   $scope.$root.$applyAsync(function () {
-                    self.table = _.merge({data: rows}, settings);
                   });
                 }
               }
@@ -94,6 +93,7 @@ module.exports = function (app) {
             oReq.responseType = 'arraybuffer';
 
             oReq.onload = function (e) {
+              console.time('parse');
               var arraybuffer = oReq.response;
 
               /* convert data to binary string */
@@ -106,8 +106,33 @@ module.exports = function (app) {
               var bstr = arr.join('');
               /* Call XLSX */
               var workbook = XLSX.read(bstr, {type: 'binary'});
+              console.timeEnd('parse');
               /* DO SOMETHING WITH workbook HERE */
-              console.log(workbook);
+
+              console.time('transform');
+              var key = Object.keys(workbook.Sheets)[0];
+              var dataSheet = sheet_to_table(workbook.Sheets[key]);
+              var rows = dataSheet.rows;
+              var headers = dataSheet.headers;
+              console.timeEnd('transform');
+
+              var settings = {
+                height: 396,
+                rowHeaders: true,
+                stretchH: 'all',
+                columnSorting: true,
+                contextMenu: false,
+                className: 'htCenter htMiddle',
+                readOnly: true,
+                colHeaders: headers
+              };
+              self.table = settings;
+              self.tableItems = rows;
+              if (!$scope.$$phase) {
+                $scope.$root.$applyAsync(function () {
+                  // self.table = _.merge({data: rows}, settings);
+                });
+              }
             };
 
             oReq.send();
@@ -116,27 +141,152 @@ module.exports = function (app) {
       }
     ]);
 
+  function sheet_to_table(sheet, opts) {
+    var val, row, range, header = 0, offset = 1, r, hdr = [], isempty, R, C, v;
+    var o = opts != null ? opts : {};
+    var raw = o.raw;
+    if (sheet == null || sheet['!ref'] == null) return [];
+    range = o.range !== undefined ? o.range : sheet['!ref'];
+    if (o.header === 1) header = 1;
+    else if (o.header === 'A') header = 2;
+    else if (Array.isArray(o.header)) header = 3;
+    switch (typeof range) {
+      case 'string':
+        r = safe_decode_range(range);
+        break;
+      case 'number':
+        r = safe_decode_range(sheet['!ref']);
+        r.s.r = range;
+        break;
+      default:
+        r = range;
+    }
+    if (header > 0) offset = 0;
+    var rr = encode_row(r.s.r);
+    var cols = new Array(r.e.c - r.s.c + 1);
+    var out = new Array(r.e.r - r.s.r - offset + 1);
+    var outi = 0;
+    for (C = r.s.c; C <= r.e.c; ++C) {
+      cols[C] = encode_col(C);
+      val = sheet[cols[C] + rr];
+      switch (header) {
+        case 1:
+          hdr[C] = C;
+          break;
+        case 2:
+          hdr[C] = cols[C];
+          break;
+        case 3:
+          hdr[C] = o.header[C - r.s.c];
+          break;
+        default:
+          if (val === undefined) continue;
+          hdr[C] = format_cell(val);
+      }
+    }
 
-  //app.factory("XLSXReaderService", ['$q', '$rootScope',
-  //  function($q, $rootScope) {
-  //    var service = function(data) {
-  //      angular.extend(this, data);
-  //    };
-  //
-  //    service.readFile = function(file, showPreview) {
-  //      var deferred = $q.defer();
-  //
-  //      XLSXReader(file, showPreview, function(data){
-  //        $rootScope.$apply(function() {
-  //          deferred.resolve(data);
-  //        });
-  //      });
-  //
-  //      return deferred.promise;
-  //    };
-  //
-  //    return service;
-  //  }
-  //]);
+    for (R = r.s.r + offset; R <= r.e.r; ++R) {
+      rr = encode_row(R);
+      isempty = true;
+      row = [];
+      //if (header === 1) row = [];
+      //else {
+      //  row = {};
+      //  if (Object.defineProperty) Object.defineProperty(row, '__rowNum__', {value: R, enumerable: false});
+      //  else row.__rowNum__ = R;
+      //}
 
+      for (C = r.s.c; C <= r.e.c; ++C) {
+        val = sheet[cols[C] + rr];
+        if (val === undefined || val.t === undefined) continue;
+        v = val.v;
+        switch (val.t) {
+          case 'e':
+            continue;
+          case 's':
+            break;
+          case 'b':
+          case 'n':
+            break;
+          default:
+            throw 'unrecognized type ' + val.t;
+        }
+        if (v !== undefined) {
+          row[C] = raw ? v : format_cell(val, v);
+          //row[hdr[C]] = raw ? v : format_cell(val, v);
+          isempty = false;
+        }
+      } // end column
+      if (isempty === false || header === 1) out[outi++] = row;
+    } // end row
+    out.length = outi;
+    return {rows: out, headers: hdr};
+  }
+
+  // utils copy of privates from XLSX
+  function safe_decode_range(range) {
+    var o = {s: {c: 0, r: 0}, e: {c: 0, r: 0}};
+    var idx = 0, i = 0, cc = 0;
+    var len = range.length;
+    for (idx = 0; i < len; ++i) {
+      if ((cc = range.charCodeAt(i) - 64) < 1 || cc > 26) break;
+      idx = 26 * idx + cc;
+    }
+    o.s.c = --idx;
+
+    for (idx = 0; i < len; ++i) {
+      if ((cc = range.charCodeAt(i) - 48) < 0 || cc > 9) break;
+      idx = 10 * idx + cc;
+    }
+    o.s.r = --idx;
+
+    if (i === len || range.charCodeAt(++i) === 58) {
+      o.e.c = o.s.c;
+      o.e.r = o.s.r;
+      return o;
+    }
+
+    for (idx = 0; i != len; ++i) {
+      if ((cc = range.charCodeAt(i) - 64) < 1 || cc > 26) break;
+      idx = 26 * idx + cc;
+    }
+    o.e.c = --idx;
+
+    for (idx = 0; i != len; ++i) {
+      if ((cc = range.charCodeAt(i) - 48) < 0 || cc > 9) break;
+      idx = 10 * idx + cc;
+    }
+    o.e.r = --idx;
+    return o;
+  }
+
+  function encode_row(row) {
+    return '' + (row + 1);
+  }
+
+  function encode_col(col) {
+    var s = '';
+    for (++col; col; col = Math.floor((col - 1) / 26)) s = String.fromCharCode(((col - 1) % 26) + 65) + s;
+    return s;
+  }
+
+  function safe_format_cell(cell, v) {
+    if (cell.z !== undefined) try {
+      return (cell.w = SSF.format(cell.z, v));
+    } catch (e) {
+    }
+    if (!cell.XF) return v;
+    try {
+      return (cell.w = SSF.format(cell.XF.ifmt || 0, v));
+    } catch (e) {
+      return '' + v;
+    }
+  }
+
+  function format_cell(cell, v) {
+    if (cell == null || cell.t == null) return "";
+    if (cell.w !== undefined) return cell.w;
+    if (v === undefined) return safe_format_cell(cell, cell.v);
+    return safe_format_cell(cell, v);
+  }
 };
