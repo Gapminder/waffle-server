9361e24c417f6380b9fa28ddb7be4c69e7b73999
buchslava <vyacheslav.chub@gmail.com>
2015-12-01 18:40:49 +0200

    Updated: aws s3 service: cors, file fields (backend) & small config fix (frontend) #33

--- ws.public2/src/config.ts
+++ ws.public2/src/config.ts
@@ -1,3 +1,3 @@
 export const USER_LOGIN_URL = 'http://localhost:8001/auth';
 export const SERVER_URL = 'http://localhost:5000';
-export const UPLOAD_URL = SERVER_URL + '/api/files/uploads-new';
+export const UPLOAD_URL = SERVER_URL + '/api/files/uploads';
