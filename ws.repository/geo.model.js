e33412c48f87908c3cf7482eec8e90e3d29f1970
Dmitriy Shekhovtsov <valorkin@gmail.com>
2015-12-04 15:43:41 +0200

    new: added geo model

---
+++ ws.repository/geo.model.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Geo = new Schema({
+  // description
+  gid: {type: String, match: /^[a-z]+[a-z0-9_]*$/, index: true, unique: true, required: true },
+  name: String,
+  description: String,
+
+  // location
+  lat: Number,
+  lng: Number,
+
+  // categories (populate via gid)?
+  geoRegion4: String,
+  geoWestRest: String,
+
+  // color should not be here
+  color: String,
+
+  // planet, g_region4
+  subdim: String,
+
+  // geo category
+  isRegion4: Boolean,
+  isTerritory: Boolean,
+  isUnState: Boolean
+});
+
+mongoose.model('Geo', Geo);
