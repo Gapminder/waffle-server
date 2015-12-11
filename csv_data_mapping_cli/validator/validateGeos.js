4a83b48a3a13b8cad26938eb8b0f06f2d4941ea4
Georgii Rychko <georgii.rychko@valor-software.com>
2015-12-11 20:38:15 +0200

    Update: refactor validation logic, improve processing speed

--- csv_data_mapping_cli/validator/validateGeos.js
+++ csv_data_mapping_cli/validator/validateGeos.js
@@ -1,182 +1,189 @@
-var Converter = require('csvtojson').Converter;
-var _ = require('lodash');
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
-var mongoose = require('mongoose');
+'use strict';
+
+let _ = require('lodash');
+let fs = require('fs');
+let path = require('path');
+let async = require('async');
+let mongoose = require('mongoose');
+let Converter = require('csvtojson').Converter;
 
 mongoose.connect('mongodb://localhost:27017/ws_test');
-var Geo = require('../../ws.repository/geo.model');
+let Geo = require('../../ws.repository/geo.model');
 
-var vizabiToWsGeoProperties = {
-  'geo': 'gid',
-  'geo.name': 'name',
-  'geo.cat': 'subdim',
-  'geo.region': 'getRegion4',
-  'geo.lat': 'lat',
-  'geo.lng': 'lng'
-};
-
-var wsToVizabiGeoProperties = _.invert(vizabiToWsGeoProperties);
-
-var geoValidationTasks = [
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/dont-panic-poverty-geo-properties.csv'),
-    headers: ['geo','geo.name','geo.cat','geo.region','geo.lat','geo.lng']
-  },
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/usa-geo-properties.csv'),
-    headers: ['geo','geo.name','geo.cat','geo.region']
-  },
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/basic-indicators-geo-properties.csv'),
-    headers: ['geo', 'geo.name', 'geo.cat', 'geo.region']
-  },
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/basic-indicators.csv'),
-    headers: ['geo']
+console.time('validationTime');
+Geo.find({}, {__v: 0, _id: 0, isUnState: 0, isTerritory: 0, geoWestRest: 0}).lean().exec((err, geos) => {
+  if (err) {
+    throw err;
   }
-];
 
-geoValidationTasks = geoValidationTasks.concat(_.range(1800, 2016).map((year) => {
-  return {
-    file: path.resolve(__dirname, `../data/vizabi/waffles/dont-panic-poverty-${year}.csv`),
-    headers: ['geo']
+  let allWsGeos = _.indexBy(geos, 'gid');
+
+  let vizabiToWsGeoProperties = {
+    'geo': 'gid',
+    'geo.name': 'name',
+    'geo.cat': 'subdim',
+    'geo.region': 'geoRegion4',
+    'geo.lat': 'lat',
+    'geo.lng': 'lng'
   };
-}));
 
-geoValidationTasks = geoValidationTasks.concat([
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/dont-panic-poverty.csv'),
-    headers: ['geo']
-  },
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/dont-panic-poverty-withmini.csv'),
-    headers: ['geo']
-  },
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/swe.csv'),
-    headers: ['geo']
-  },
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/usa.csv'),
-    headers: ['geo']
-  },
-  {
-    file: path.resolve(__dirname, '../data/vizabi/waffles/basic-indicators.csv'),
-    headers: ['geo']
+  function mapHeadersToWsCompatible(headers) {
+    return _.map(headers, header => vizabiToWsGeoProperties[header])
   }
-]);
 
-async.parallelLimit(_.map(geoValidationTasks,
-  (task) =>
-    (cb) => {
-      validateGeos(task, groupedByGidGeoResults => {
-        console.log(`processed file ${task.file}`);
-        cb(null, groupedByGidGeoResults)
-      })
-    }), 1,
-  (err, results) => {
-    console.log(
-    _.reduce(results, (result, next) => {
-      return _.merge(result, next, function(a, b) {
+  let geoValidationTasks = [
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/dont-panic-poverty-geo-properties.csv'),
+      headers: mapHeadersToWsCompatible(['geo','geo.name','geo.cat','geo.region','geo.lat','geo.lng'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/usa-geo-properties.csv'),
+      headers: mapHeadersToWsCompatible(['geo','geo.name','geo.cat','geo.region'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/basic-indicators-geo-properties.csv'),
+      headers: mapHeadersToWsCompatible(['geo', 'geo.name', 'geo.cat', 'geo.region'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/basic-indicators.csv'),
+      headers: mapHeadersToWsCompatible(['geo'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/dont-panic-poverty.csv'),
+      headers: mapHeadersToWsCompatible(['geo'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/dont-panic-poverty-withmini.csv'),
+      headers: mapHeadersToWsCompatible(['geo'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/swe.csv'),
+      headers: mapHeadersToWsCompatible(['geo'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/usa.csv'),
+      headers: mapHeadersToWsCompatible(['geo'])
+    },
+    {
+      file: path.resolve(__dirname, '../data/vizabi/waffles/basic-indicators.csv'),
+      headers: mapHeadersToWsCompatible(['geo'])
+    }
+  ];
+
+  geoValidationTasks = geoValidationTasks.concat(_.range(1800, 2016).map(year => {
+    return {
+      file: path.resolve(__dirname, `../data/vizabi/waffles/dont-panic-poverty-${year}.csv`),
+      headers: mapHeadersToWsCompatible(['geo'])
+    };
+  }));
+
+  let geoValidators = _.map(geoValidationTasks, validateGeos);
+
+  async.parallelLimit(geoValidators, 1,
+    (err, groupedByGidGeoResults) => {
+      console.log(mergeGroupedByGidGeoResults(groupedByGidGeoResults));
+      console.timeEnd('validationTime');
+  });
+
+  function mergeGroupedByGidGeoResults(groupedByGidGeoResults) {
+    let reduceInitialValue = {};
+    return _.reduce(groupedByGidGeoResults, (groupedByGidGeoResultPeace, next) => {
+      return _.merge(groupedByGidGeoResultPeace, next, (a, b) => {
         if (_.isArray(a)) {
           return a.concat(b);
         }
       });
-    }, {}));
-    console.log('Validation is completed.')
-});
+    }, reduceInitialValue);
+  }
 
-function validateGeos(geoValidationTask, onValidationComplete) {
-  async.waterfall(
-    [
-      //Parse csv with geos
-      cb => parseCsvFile(geoValidationTask.file, geoValidationTask.headers, (err, csvRecords) => {
-        return cb(err, csvRecords);
-      }),
-      //Map each record into comparison actions in order to detect diffs
-      (csvRecords, cb) => {
-        var comparisonActions = _.map(csvRecords, (geoJson, rowNum) => {
-          return createComparisonAction(geoValidationTask.file, geoJson, rowNum);
+  function validateGeos(geoValidationTask) {
+    return onValidationComplete => {
+      async.waterfall(
+        [
+          //Parse csv with geos
+          cb => parseCsvFile(geoValidationTask.file, geoValidationTask.headers, cb),
+          //Map each record into comparison actions in order to detect diffs
+          (csvRecords, cb) => {
+            let comparisonActions = _.map(csvRecords, (geoJson, rowNum) => {
+              return createComparisonAction(geoValidationTask, geoJson, rowNum);
+            });
+            return cb(null, comparisonActions);
+          },
+          //Execute comparison actions and group them by gid
+          (comparisonActions, cb) => {
+            async.series(comparisonActions, (err, comparedGeos) => {
+              let groupedByGidGeoResults =
+                _.chain(comparedGeos)
+                  .filter() // empty filter will get rid of undefined
+                  .groupBy(comparedGeo => comparedGeo.gid)
+                  .value();
+
+              return cb(err, groupedByGidGeoResults);
+            });
+          }
+        ],
+        //Execute callback on validation complete
+        (err, groupedByGidGeoResults) => {
+          if (err) {
+            return onValidationComplete(err);
+          }
+
+          console.log(`processed file ${geoValidationTask.file}`);
+          return onValidationComplete(null, groupedByGidGeoResults);
         });
-        return cb(null, comparisonActions);
-      },
-     //Execute comparison actions and group them by gid
-      (comparisonActions, cb) => {
-        async.parallelLimit(comparisonActions, 200, (err, comparedGeos) => {
-          cb(err, _.groupBy(_.filter(comparedGeos, onlyExistingGeos()), (comparedGeo) => comparedGeo.gid));
-        });
-      }
-    ],
-    //Execute callback on validation complete
-    (err, groupedByGidGeoResults) => {
-      if (err) {
-        console.error(err);
-      }
-      onValidationComplete(groupedByGidGeoResults);
-    });
-}
+    }
+  }
 
-function createComparisonAction(csvFile, geoJson, rowNum) {
-  return (cb) => {
-    return Geo.findOne(prepareGeoQuery(geoJson)).lean().exec((err, geo) => {
-      if (err) {
-        return cb(err);
-      }
+  function createComparisonAction(task, geoJson, rowNum) {
+    return cb => {
+      let gid = geoJson.gid;
+      let wsGeo = allWsGeos[gid];
+      let diff = getDifference(task.headers, geoJson, wsGeo);
 
-      var diff = getDifference(geoJson, geo);
-      if (!geo || diff) {
+      if (!wsGeo || diff.length) {
         return cb(null, {
-          file: csvFile,
+          file: task.file,
           vizabiCsvRowNum: rowNum + 2,
-          gid: geo && geo.gid ? geo.gid : 'ABSENT_IN_WS: '+ geoJson[wsToVizabiGeoProperties['gid']],
+          gid: wsGeo && wsGeo.gid ? wsGeo.gid : `ABSENT_IN_WS: ${gid}`,
           diff: JSON.stringify(diff)
         });
       }
 
       // In case nothing to compare 'undefined' will be emitted.
       return cb();
-    });
-  }
-}
-
-function prepareGeoQuery(geoJson) {
-  return {gid: geoJson[wsToVizabiGeoProperties['gid']]};
-}
-
-function getDifference(vizabiCsvGeo, wsGeo) {
-  var diff = [];
-  _.keys(wsGeo).forEach(function(wsGeoProp) {
-    if (wsToVizabiGeoProperties[wsGeoProp] && wsGeo[wsGeoProp] != vizabiCsvGeo[wsToVizabiGeoProperties[wsGeoProp]]) {
-      diff.push({
-        [wsGeoProp]: wsGeo[wsGeoProp] ? wsGeo[wsGeoProp] : 'ABSENT_IN_WS',
-        [wsToVizabiGeoProperties[wsGeoProp]]: vizabiCsvGeo[wsToVizabiGeoProperties[wsGeoProp]] ? vizabiCsvGeo[wsToVizabiGeoProperties[wsGeoProp]] : 'ABSENT_IN_VIZABI'
-      });
     }
-  });
+  }
 
-  if (diff.length) {
+  function getDifference(headers, vizabiGeo, wsGeo) {
+    let diff = [];
+    _.chain(_.keys(wsGeo))
+      .filter(prop => headers.indexOf(prop) !== -1)
+      .each(prop => {
+        let wsValue = wsGeo[prop];
+        let vizabiValue = vizabiGeo[prop];
+
+        if (wsValue != vizabiValue) {
+          diff.push({
+            [prop]: wsValue ? wsValue : 'ABSENT_IN_WS',
+            [`${prop}(vizabi)`]: vizabiValue ? vizabiValue : 'ABSENT_IN_VIZABI'
+          });
+        }
+      })
+      .value();
     return diff;
   }
 
-  return null;
-}
+  function parseCsvFile(file, headers, cb) {
+    let converter = new Converter({
+      workerNum: 4,
+      headers: headers,
+      flatKeys: true
+    });
 
-function onlyExistingGeos() {
-  return v => v;
-}
+    converter.on('end_parsed', jsonArray => {
+      return cb(null, jsonArray);
+    });
 
-function parseCsvFile(file, headers, cb) {
-  var converter = new Converter({
-    workerNum: 4,
-    headers: headers,
-    flatKeys: true
-  });
-
-  converter.on('end_parsed', function (jsonArray) {
-    return cb(null, jsonArray);
-  });
-
-  fs.createReadStream(file).pipe(converter);
-}
+    fs.createReadStream(file).pipe(converter);
+  }
+});
--- csv_data_mapping_cli/validator/validateMeasureValues.js
+++ csv_data_mapping_cli/validator/validateMeasureValues.js
@@ -1,14 +1,16 @@
-var Converter = require('csvtojson').Converter;
-var _ = require('lodash');
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
-var mongoose = require('mongoose');
+'use strict';
+
+let _ = require('lodash');
+let fs = require('fs');
+let path = require('path');
+let async = require('async');
+let mongoose = require('mongoose');
+let Converter = require('csvtojson').Converter;
 
 mongoose.connect('mongodb://localhost:27017/ws_test');
-var IndicatorsValues = require('../../ws.repository/indicator-values/indicator-values.model');
+let IndicatorsValues = require('../../ws.repository/indicator-values/indicator-values.model');
 
-var measureValuesTasks = _.range(1800, 2016).map((year) => {
+let measureValuesTasks = _.range(1800, 2016).map((year) => {
   return {
     file: path.resolve(__dirname, `../data/vizabi/waffles/dont-panic-poverty-${year}.csv`),
     headers: ['geo','time','pop','gdp_per_cap','gini','u5mr'],
@@ -55,59 +57,59 @@ measureValuesTasks = measureValuesTasks.concat([
   }
 ]);
 
-async.parallelLimit(_.map(measureValuesTasks,
-  (task) =>
-    (cb) => {
-      validateMeasureValues(task, groupedByMeasureResults => {
-        console.log(`processed file ${task.file}`);
-        console.log('diff: ', groupedByMeasureResults);
-        cb(null, groupedByMeasureResults)
-      })
-    }), 1, err => console.log('Validation is completed.'));
+let measureValuesValidators = _.map(measureValuesTasks, validateMeasureValues);
 
-function validateMeasureValues(measureValuesValidationTask, onValidationComplete) {
-  async.waterfall(
-    [
-      //Parse csv with measure values
-      cb => parseCsvFile(measureValuesValidationTask.file, measureValuesValidationTask.headers, (err, csvRecords) => {
-        return cb(err, csvRecords);
-      }),
-      //Map each record into comparison actions in order to detect diffs
-      (csvRecords, cb) => {
-        var comparisonActions = _.map(csvRecords, (measureJson, rowNum) => {
-          return _.map(measureValuesValidationTask.measureNames, (measureName) =>
-            createComparisonAction(measureName, measureJson, measureValuesValidationTask.file, rowNum));
-        });
-        return cb(null, _.flatten(comparisonActions));
-      },
-      //Execute comparison actions and group them by measure name
-      (comparisonActions, cb) => {
-        async.parallelLimit(comparisonActions, 200, (err, comparedMeasureValues) => {
-          cb(err, _.groupBy(_.filter(comparedMeasureValues, onlyExistingValues()), (measure) => measure.indicator));
-        });
-      }
-    ],
-    //Execute callback on validation complete.
-    (err, groupedByMeasureNameValues) => {
-      if (err) {
-        console.error(err);
-      }
-      onValidationComplete(groupedByMeasureNameValues)
-    });
-}
+console.time('validationTime');
+async.parallelLimit(measureValuesValidators, 1, err => console.timeEnd('validationTime'));
 
-function onlyExistingValues() {
-  return v => v;
+function validateMeasureValues(measureValuesValidationTask) {
+  return onValidationComplete => {
+    async.waterfall(
+      [
+        //Parse csv with measure values
+        cb => parseCsvFile(measureValuesValidationTask.file, measureValuesValidationTask.headers, cb),
+        //Map each record into comparison actions in order to detect diffs
+        (csvRecords, cb) => {
+          let comparisonActions = _.map(csvRecords, (measureJson, rowNum) => {
+            return _.map(measureValuesValidationTask.measureNames,
+              (measureName) => createComparisonAction(measureName, measureJson, measureValuesValidationTask.file, rowNum));
+          });
+          return cb(null, _.flatten(comparisonActions));
+        },
+        //Execute comparison actions and group them by measure name
+        (comparisonActions, cb) => {
+          async.parallelLimit(comparisonActions, 200, (err, comparedMeasureValues) => {
+            let groupedByMeasureNameValues =
+              _.chain(comparedMeasureValues)
+              .filter()//filters out undefined
+              .groupBy(measure => measure.indicator)
+              .value();
+
+            return cb(err, groupedByMeasureNameValues);
+          });
+        }
+      ],
+      //Execute callback on validation complete.
+      (err, groupedByMeasureNameValues) => {
+        if (err) {
+          return onValidationComplete(err)
+        }
+
+        console.log(`processed file ${measureValuesValidationTask.file}`);
+        console.log('diff: ', groupedByMeasureNameValues);
+        return onValidationComplete(null, groupedByMeasureNameValues)
+      });
+  }
 }
 
 function parseCsvFile(file, headers, cb) {
-  var converter = new Converter({
+  let converter = new Converter({
     workerNum: 4,
     headers: headers,
     flatKeys: true
   });
 
-  converter.on('end_parsed', function (jsonArray) {
+  converter.on('end_parsed', jsonArray => {
     return cb(null, jsonArray);
   });
 
@@ -115,13 +117,16 @@ function parseCsvFile(file, headers, cb) {
 }
 
 function createComparisonAction(measureName, measureJson, file, rowNum) {
-  return (cb) => {
-    return IndicatorsValues.findOne(prepareMeasureValueQuery(measureName, measureJson)).lean().exec((err, indicator) => {
+  return cb => {
+    let measureValuesQuery = prepareMeasureValueQuery(measureName, measureJson);
+
+    return IndicatorsValues.findOne(measureValuesQuery).lean().exec((err, indicator) => {
       if (err) {
         return cb(err);
       }
 
-      if (!indicator || measureJson[measureName] != indicator.value) {
+      let vizabiMeasureValue = measureJson[measureName];
+      if (!indicator || vizabiMeasureValue != indicator.value) {
         return cb(null, {
           file: file,
           rowNum: rowNum + 2,
@@ -129,10 +134,10 @@ function createComparisonAction(measureName, measureJson, file, rowNum) {
           gid: measureJson.geo,
           indicator: measureName,
           wsValue: indicator ? indicator.value : 'ABSENT_IN_WS',
-          vizabiValue: measureJson[measureName] ? measureJson[measureName] : 'ABSENT_IN_VIZABI'
+          vizabiValue: vizabiMeasureValue ? vizabiMeasureValue : 'ABSENT_IN_VIZABI'
         });
       }
-      // In case nothing to compare 'undefined' will be emitted.
+      // In case nothing to compare - 'undefined' will be emitted.
       return cb();
     });
   }
