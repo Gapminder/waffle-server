b9ed78afaf885e5dda8f6bc6685cdc36e5537291
Dmitriy Shekhovtsov <valorkin@gmail.com>
2015-12-29 18:19:59 +0200

    fix: move adapter out of api folder

--- ws.routes/index.js
+++ ws.routes/index.js
@@ -11,7 +11,7 @@ module.exports = function (serviceLocator) {
   require('./api/publisher-catalogs')(serviceLocator);
   require('./api/publisher-catalog-versions')(serviceLocator);
   require('./api/charts')(serviceLocator);
-  require('./api/adapter')(serviceLocator);
+  require('./adapter')(serviceLocator);
   require('./geo')(serviceLocator);
 
   require('./auth')(serviceLocator);
