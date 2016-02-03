d7b17ddb22bb8e4f7bfa6078b0d64f10c96289bc
natali sol <natali.abyss@gmail.com>
2016-02-03 11:27:15 +0200

    Fix: exclude node_modules from test path

--- gulp/mocha.js
+++ gulp/mocha.js
@@ -4,5 +4,5 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
 gulp.task('test', () => {
-  return gulp.src('**/*.spec.js', {read: false}).pipe(mocha());
+  return gulp.src(['!node_modules/**','**/*.spec.js'], {read: false}).pipe(mocha());
 });
