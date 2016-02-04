d5c30a308d91bffa201773c06c97caf81ef928e9
georgii.rychko <georgii.rychko@valor-software.com>
2016-02-04 10:35:04 +0200

    test(stats endpoint) - add basic integration tests for stats endpoint

--- gulp/mocha.js
+++ gulp/mocha.js
@@ -4,5 +4,13 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
 gulp.task('test', () => {
-  return gulp.src(['!node_modules/**','**/*.spec.js'], {read: false}).pipe(mocha());
+  return runMochaOn(['!**/*integ.spec.js', '**/*.spec.js']);
 });
+
+gulp.task('test:integ', () => {
+  return runMochaOn(['**/*integ.spec.js']);
+});
+
+function runMochaOn(path) {
+  return gulp.src(['!node_modules/**'].concat(path), {read: false}).pipe(mocha());
+}
