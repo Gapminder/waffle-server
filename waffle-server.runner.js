6680da398b6b7f400136406273582adaaad35c2e
Oleksandra korel Kalinina <korery@gmail.com>
2016-03-28 19:19:11 +0300

    feat(shelljs-runners): rewrite runners from bash to shelljs
    - importer ddf,
    - importer metadata,
    - exporter,
    - WS server,
    - swagger-ui server
    
    Closes #184

---
+++ waffle-server.runner.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const shell = require('shelljs');
+const utils = require('./utils.runner');
+
+const NODE_ENV = utils.config.NODE_ENV;
+const LOG_LEVEL = utils.config.LOG_LEVEL;
+const LOG_TRANSPORTS = utils.config.LOG_TRANSPORTS;
+const SESSION_SECRET = utils.config.SESSION_SECRET;
+const GOOGLE_CLIENT_ID = utils.config.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = utils.config.GOOGLE_CLIENT_SECRET;
+const MONGODB_URL = utils.config.MONGODB_URL;
+const NEO4J_URL = utils.config.NEO4J_URL;
+
+const PARAMS = `${NODE_ENV} ${LOG_LEVEL} ${LOG_TRANSPORTS} ${SESSION_SECRET} ${GOOGLE_CLIENT_ID} ${GOOGLE_CLIENT_SECRET} ${MONGODB_URL} ${NEO4J_URL}`;
+
+utils.assertCommandExists('forever');
+utils.assertCommandExists('redis-cli');
+
+shell.exec('forever stopall');
+shell.echo('\nRedis flash all data process:');
+shell.exec('redis-cli flushall');
+shell.echo('\nRedis flash db process:');
+shell.exec('redis-cli flushdb');
+shell.exec(`${PARAMS} npm start`);
+shell.exec('forever list');
+
+shell.exit(0);
+
