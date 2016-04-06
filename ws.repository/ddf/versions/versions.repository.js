3a2a74566fabc18e5cd1e035bce9760ad563ae3e
georgii.rychko <georgii.rychko@valor-software.com>
2016-04-06 13:14:32 +0300

    feat(ddf models): add Versions mongoose model

---
+++ ws.repository/ddf/versions/versions.repository.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Versions = mongoose.model('Versions');
+const utils = require('../../utils');
+
+function VersionsRepository() {
+}
+
+['pagedList', 'update', 'findById', 'deleteRecord'].forEach(actionName => {
+  VersionsRepository.prototype[actionName] = utils.actionFactory(actionName)(Versions, this);
+});
+
+module.exports = VersionsRepository;
