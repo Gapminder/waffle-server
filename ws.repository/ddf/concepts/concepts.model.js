923fc534b598b50c9e11f343c2b33f9931957658
georgii.rychko <georgii.rychko@valor-software.com>
2016-04-06 16:40:03 +0300

    doc(ddf model): add jsdoc for mongo models

--- ws.repository/ddf/concepts/concepts.model.js
+++ ws.repository/ddf/concepts/concepts.model.js
@@ -3,19 +3,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/**
+ * @typedef CoordinateSchema
+ * @memberof Concepts
+ * @class
+ *
+ * @param {String} gid - concept identificator
+ * @param {String} name - human-readable name of the concept
+ * @param {String} tooltip - additional information regarding concept
+ * @param {String} indicatorUrl - url that points to concept definition and explanation
+ * @param {String} domain - name of the domain concept belongs to
+ * @param {String} type - can be on of the following: 'entity_set', 'entity_domain', 'time', 'string', 'measure'. By default 'string' will be used
+ * @param {Object} previous - a link to previous version of the current entity 
+ */
 const ConceptsSchema = new Schema({
-  value: String,
+  gid: String,
   name: String,
-  type: { type: String, enum: ['entity_set', 'entity_domain', 'time', 'string', 'measure'], default: 'string'},
+  type: { type: String, enum: ['entity_set', 'entity_domain', 'time', 'string', 'measure'], 'default': 'string'},
   tooltip: String,
   indicatorUrl: String,
   domain: String,
 
-  previousValue: {type: Schema.Types.ObjectId, ref: 'Concepts', sparse: true}
+  previous: {type: Schema.Types.ObjectId, ref: 'Concepts', sparse: true}
 });
 
 ConceptsSchema.index({type: 1});
 ConceptsSchema.index({domain: 1});
-ConceptsSchema.index({value: 1, type: 1});
+ConceptsSchema.index({gid: 1, type: 1});
 
 module.exports = mongoose.model('Concepts', ConceptsSchema);
--- ws.repository/ddf/data-set-versions/data-set-versions.model.js
+++ ws.repository/ddf/data-set-versions/data-set-versions.model.js
@@ -7,19 +7,21 @@ const Schema = mongoose.Schema;
  * @typedef {Object} DataSetVersions
  * @memberof Models
  *
+ * @property {Object} value - version's value
+ * @property {String} status - signifies state of the version - might be one of the following: 'IMPORTED', 'PUBLISHED', 'WORK_IN_PROGRESS'. By default 'WORK_IN_PROGRESS' is used
  * @property {Object} isCurrent - the version is now in neo4j
-
  * @property {Models.User} createdBy - user who added this Data Set entry
  * @property {Date} createdAt - timestamp when this DataSet was created
+ * @property {Object} basedOn - version from which current version is originate or in other words - parent version or origin.
  */
 const VersionsSchema = new Schema({
   value: String,
-  status: { type: String, enum: ['IMPORTED', 'PUBLISHED', 'WORK_IN_PROGRESS'], default: 'WORK_IN_PROGRESS'},
+  status: { type: String, enum: ['IMPORTED', 'PUBLISHED', 'WORK_IN_PROGRESS'], 'default': 'WORK_IN_PROGRESS'},
 
   isCurrent: Boolean,
 
   createdBy: {type: Schema.Types.ObjectId, ref: 'Users', required: true},
-  createdAt: {type: Date, 'default': new Date(), required: true}
+  createdAt: {type: Date, 'default': new Date(), required: true},
 
   basedOn: {type: Schema.Types.ObjectId, ref: 'Versions'}
 });
