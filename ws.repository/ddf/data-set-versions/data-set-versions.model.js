dd97d0140f6b4de3c5d5b347a2ad8e9a54c6e35a
Oleksandra korel Kalinina <korery@gmail.com>
2016-04-08 18:01:35 +0300

    update

--- ws.repository/ddf/data-set-versions/data-set-versions.model.js
+++ ws.repository/ddf/data-set-versions/data-set-versions.model.js
@@ -9,10 +9,14 @@ const Schema = mongoose.Schema;
  *
  * @property {Object} value - version's value
  * @property {String} status - signifies state of the version - might be one of the following: 'IMPORTED', 'PUBLISHED', 'WORK_IN_PROGRESS'. By default 'WORK_IN_PROGRESS' is used
+ *
  * @property {Object} isCurrent - the version is now in neo4j
+ *
  * @property {Models.User} createdBy - user who added this Data Set entry
  * @property {Date} createdAt - timestamp when this DataSet was created
- * @property {Object} basedOn - version from which current version is originate or in other words - parent version or origin.
+ *
+ * @property {Models.DataSets} dataset - of version, could be only one
+ * @property {Models.DataSetVersions} basedOn - version from which current version is originate or in other words - parent version or origin.
  */
 const DataSetVersions = new Schema({
   value: String,
@@ -23,7 +27,7 @@ const DataSetVersions = new Schema({
   createdBy: {type: Schema.Types.ObjectId, ref: 'Users', required: true},
   createdAt: {type: Date, 'default': new Date(), required: true},
 
-  dataSet: {type: Schema.Types.ObjectId, ref: 'DataSets'},
+  dataset: {type: Schema.Types.ObjectId, ref: 'DataSets'},
   basedOn: {type: Schema.Types.ObjectId, ref: 'DataSetVersions'}
 });
 
