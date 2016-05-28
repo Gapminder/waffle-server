6c5ab9fb272bbffa1a66717d5672711495afbbc1
Alex Tsap <komod.kharkiv.alex@gmail.com>
2016-05-28 16:09:27 +0300

    feat(Repos-route): create route for repositories

--- ws.routes/ddf/import/repos.controller.js
+++ ws.routes/ddf/import/repos.controller.js
@@ -1,26 +1,38 @@
 'use strict';
 
-var express = require('express');
-var shell = require('shelljs');
+const _ = require('lodash');
+
+const express = require('express');
+const shell = require('shelljs');
+const fs = require('fs');
+const path = require('path');
 
 let gitHubUrl = 'git@github.com:valor-software/ddf--gapminder_world-stub-1.git';
 let gitCommit = 'aafed7d4dcda8d736f317e0cd3eaff009275cbb6';
+let dirForRepos = 'csv_data_mapping_cli/repos';
 
 module.exports = serviceLocator => {
-  let app = serviceLocator.getApplication();
-  var router = express.Router();
+  const app = serviceLocator.getApplication();
+  const logger = app.get('log');
+  const router = express.Router();
 
   router.get('/api/ddf/import/repo', cloneRepositories);
   return app.use(router);
+
+  function cloneRepositories(req, res, next) {
+    const pathToRepo = path.resolve(process.cwd(), dirForRepos, getDirName());
+    fs.exists(pathToRepo, exists => {
+      if (!exists) {
+        shell.exec(`git clone ${gitHubUrl} ./${dirForRepos}/${getDirName()}`);
+      }
+      logger.warn(`Directory for cloning ${gitHubUrl} is already exist, hence repo won't be cloned`);
+      return next();
+    });
+
+    function getDirName() {
+      const partsOfGitHubUrl = gitHubUrl.split('/');
+      const lastPart = _.last(partsOfGitHubUrl);
+      return _.first(lastPart.split('.'));
+    }
+  }
 };
-
-function cloneRepositories() {
-
-  shell.exec(` git clone ${gitHubUrl} ./csv_data_mapping_cli/repos`);
-
-  // - git checkout, должна выполняться в контекст (папке) с репозиторием.
-  // - есть путь к репозиторию, нужно получить имя папки ( split + split || regexp)
-  // - вторая команда с checkout сделать по аналогии с оператором &&
-
-  shell.exec(`git checkout ${gitCommit}`);
-}
