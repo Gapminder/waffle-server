9fc9d531457ddf93a723bf5403ad21b5a91d44f4
Oleksandra korel Kalinina <korery@gmail.com>
2016-05-30 15:22:24 +0300

    feat(inc-update): add to incremental update checkingof dataset locking

--- ws.routes/ddf/demo/demo.controller.js
+++ ws.routes/ddf/demo/demo.controller.js
@@ -112,25 +112,77 @@ module.exports = (serviceLocator) => {
     });
   }
 
-  function updateIncrementally(req, res, next) {
-    Transactions.findOne({commit: req.body.commit}).lean().exec((error, transaction) => {
-      if (error) {
-        logger.error(error);
-        return res.json({success: !error, error});
+  function updateIncrementally(req, res) {
+    let pipe = {
+      commit: req.body.commit,
+      github: req.body.github,
+      datasetName: reposService.getRepoName(req.body.github),
+      diff: JSON.parse(req.body.diff)
+    };
+
+    return async.waterfall([
+      async.constant(pipe),
+      lockDataset,
+      checkTransaction,
+      runIncrementalUpdate,
+      unlockDataset
+    ], (err) => {
+      if (err) {
+        logger.error(err);
       }
 
-      if (transaction) {
-        logger.error(`Version of dataset "${req.body.github}" with commit: "${transaction.commit}" was already applied`);
-        return res.json({
-          success: false,
-          error: `Version of dataset with commit: "${transaction.commit}" was already applied`
-        });
-      }
-
-      incrementalUpdateService(app, error => {
-        return res.json({success: !error, error});
-      }, {diff: JSON.parse(req.body.diff), datasetName: reposService.getRepoName(req.body.github), commit: req.body.commit, github: req.body.github});
+      return res.json({success: !err, err});
     });
+
+    function lockDataset(pipe, done) {
+      return Datasets
+        .findOneAndUpdate({name: pipe.datasetName, isLocked: false}, {isLocked: true}, {new: 1})
+        .lean()
+        .exec((err, dataset) => {
+          if (!dataset) {
+            return done(`Version of dataset "${pipe.datasetName}" was already locked or dataset is absent`);
+          }
+
+          return done(err, pipe);
+        });
+    }
+
+    function checkTransaction(pipe, done) {
+      return Transactions
+        .findOne({commit: pipe.commit})
+        .lean()
+        .exec((err, transaction) => {
+          if (transaction) {
+            return done(`Version of dataset "${pipe.datasetName}" with commit: "${transaction.commit}" was already applied`);
+          }
+
+          return done(err, pipe);
+        });
+    }
+
+    function runIncrementalUpdate(pipe, done) {
+      let options = {
+        diff: pipe.diff,
+        datasetName: pipe.datasetName,
+        commit: pipe.commit,
+        github: pipe.github
+      };
+
+      return incrementalUpdateService(app, (err) => done(err, pipe), options);
+    }
+
+    function unlockDataset(pipe, done) {
+      return Datasets
+        .findOneAndUpdate({name: pipe.datasetName, isLocked: true}, {isLocked: false}, {new: 1})
+        .lean()
+        .exec((err, dataset) => {
+          if (!dataset) {
+            return done(`Version of dataset "${pipe.datasetName}" wasn't locked`);
+          }
+
+          return done(err, pipe);
+        });
+    }
   }
 
   function getPrestoredQueries(req, res, next) {
