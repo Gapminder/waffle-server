16f4aa32d19debff028a42d33d828cf308f03776
georgii.rychko <georgii.rychko@valor-software.com>
2016-06-13 12:45:51 +0300

    feat(concepts.repository): add method for finding concepts properties

--- ws.repository/ddf/concepts/concepts.repository.js
+++ ws.repository/ddf/concepts/concepts.repository.js
@@ -22,44 +22,29 @@ ConceptsRepositoryWrapper.prototype.currentVersion = function () {
   return new ConceptsRepository(versionQueryFragment)
 };
 
-ConceptsRepositoryWrapper.prototype.previousVersionsLeftovers = function () {
-  const versionQueryFragment = {
-    from: {$lt: this.version},
-    to: constants.MAX_VERSION,
-    dataset: this.datasetId
-  };
-  return new ConceptsRepository(versionQueryFragment)
-};
-
 function ConceptsRepository(versionQueryFragment) {
   this.versionQueryFragment = versionQueryFragment;
 }
+
+ConceptsRepository.prototype.findConceptProperties = function (select, where, onPropertiesFound) {
+  const projection = makePositiveProjectionFor(select);
+  const conceptQuery = _.merge({}, this.versionQueryFragment, prefixWithProperties(where));
+
+  return Concepts.find(conceptQuery, projection).lean().exec(onPropertiesFound);
+};
 
 ['pagedList', 'update', 'findById', 'deleteRecord'].forEach(actionName => {
   ConceptsRepository.prototype[actionName] = utils.actionFactory(actionName)(Concepts, this);
 });
 
-ConceptsRepository.prototype.findPropertyValuesByGid = function (gid, properties, onPropertiesFound) {
-  const query = _.merge({}, this.versionQueryFragment, {gid: conceptGid});
-  const projection = makePositiveProjectionFor(properties);
-
-  return Concepts.findOne(query, projection).lean().exec(onPropertiesFound);
-};
-
-ConceptsRepository.prototype.findPropertyValuesByGid = function (gid, properties, onPropertiesFound) {
-  const query = _.merge({}, this.versionQueryFragment, {gid: conceptGid});
-  const projection = makePositiveProjectionFor(properties);
-
-  return Concepts.findOne(query, projection).lean().exec(onPropertiesFound);
-};
-
+//TODO: Move this in utils that should be common across all repositories
 function makePositiveProjectionFor(properties) {
   const positiveProjectionValues = _.fill(new Array(_.size(properties)), 1);
-  return _.chain(properties)
-    .zipObject(positiveProjectionValues)
-    .mapKeys((value, property) => `properties.${property}`)
-    .merge({gid: 1})
-    .value();
+  return prefixWithProperties(_.chain(properties).zipObject(positiveProjectionValues).value());
+}
+
+function prefixWithProperties(object) {
+  return _.mapKeys(object, (value, property) => `properties.${property}`);
 }
 
 module.exports = ConceptsRepositoryWrapper;
