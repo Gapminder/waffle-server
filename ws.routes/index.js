5abe0fdcdbb17cf01836d24f710b41c089ba67a7
georgii.rychko <georgii.rychko@valor-software.com>
2016-06-29 19:11:49 +0300

    chore(codebase): remove obsolete code

--- ws.routes/index.js
+++ ws.routes/index.js
@@ -8,8 +8,6 @@ module.exports = function (serviceLocator) {
   require('./users')(serviceLocator);
   require('./swagger-jsdoc')(serviceLocator);
 
-  require('./file-management')(serviceLocator);
-  require('./pipes')(serviceLocator);
   require('./graphs')(serviceLocator);
   require('./ddf/import')(serviceLocator);
   require('./ddf/datapoints')(serviceLocator);
--- ws.routes/pipes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (serviceLocator) {
-  require('./dimensions-recognize')(serviceLocator);
-  require('./dimensions-crud')(serviceLocator);
-  require('./indicators-crud')(serviceLocator);
-};
