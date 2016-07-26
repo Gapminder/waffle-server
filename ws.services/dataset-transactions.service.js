0a316696d1bbe497d22d1f589961bbbc3286a33f
georgii.rychko <georgii.rychko@valor-software.com>
2016-07-26 13:54:49 +0300

    fix(mongo queries):  change query params order in counting entities, concepts, datapoints so that index was used

--- ws.services/dataset-transactions.service.js
+++ ws.services/dataset-transactions.service.js
@@ -137,7 +137,7 @@ function getStatusOfLatestTransactionByDatasetName(datasetName, done) {
       }
 
       const version = latestTransaction.createdAt;
-      const closedOrOpenedByVersionQuery = {dataset: dataset._id, $or: [{from: version}, {to: version}]};
+      const closedOrOpenedByVersionQuery = {$or: [{from: version}, {to: version}], dataset: dataset._id};
 
       const modifiedObjectsTasks = {
         concepts: done => Concepts.count(closedOrOpenedByVersionQuery, done),
