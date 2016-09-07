35ae9ccb222800f5e1cd9c1a85d81e1554f1ab1d
Oleksandra korel Kalinina <korery@gmail.com>
2016-09-07 16:03:20 +0300

    feat(datapoint-normalizer): add join links generation base on where clause

--- ws.routes/ddf/datapoints/datapoints.service.js
+++ ws.routes/ddf/datapoints/datapoints.service.js
@@ -42,13 +42,39 @@ function collectDatapointsByDdfql(options, onMatchedDatapoints) {
     commonService.findDefaultDatasetAndTransaction,
     getConcepts,
     mapConcepts,
-    getEntities,
+    getEntitiesByDdfql,
     normalizeQueriesToDatapointsByDdfql
   ], (error, result) => {
     console.timeEnd('finish matching DataPoints');
 
     return onMatchedDatapoints(error, result);
   });
+}
+
+function getEntitiesByDdfql(pipe, cb) {
+  return async.map(pipe.resolvedDomainsAndSetGids, _getEntitiesByDomainOrSetGid, (err, result) => {
+    pipe.entityOriginIdsGroupedByDomain = _.mapValues(result, (value) => _.map(value, constants.ORIGIN_ID));
+    pipe.entities = _.flatMap(result);
+
+    return cb(err, pipe);
+  });
+
+  function _getEntitiesByDomainOrSetGid(domainGid, mcb) {
+    const _pipe = {
+      dataset: pipe.dataset,
+      version: pipe.version,
+      domainGid: domainGid,
+      headers: [],
+      where: {}
+    };
+
+    return entitiesService.getEntities(_pipe, (err, result) => {
+      if (err) {
+        return mcb(err);
+      }
+      return mcb(err, result.entities);
+    });
+  }
 }
 
 function normalizeQueriesToDatapointsByDdfql(pipe, cb) {
@@ -60,17 +86,7 @@ function normalizeQueriesToDatapointsByDdfql(pipe, cb) {
   }
 
   const entitiesRepository = entitiesRepositoryFactory.currentVersion(pipe.dataset._id, pipe.version);
-  const conceptsByGids = _.chain(pipe.concepts)
-    .keyBy('gid')
-    .mapValues('originId')
-    .value();
-
-  const timeConcepts = _.chain(pipe.concepts)
-    .filter(concept => _.get(concept, 'properties.concept_type') === 'time')
-    .map('gid')
-    .value();
-
-  const normalizedQuery = ddfql.normalizeDatapoints(pipe.query, conceptsByGids, timeConcepts);
+  const normalizedQuery = ddfql.normalizeDatapoints(pipe.query, pipe.concepts);
 
   return async.mapLimit(normalizedQuery.join, 10, (item, mcb) => {
     return entitiesRepository.findEntityPropertiesByQuery(item, (error, entities) => mcb(error, _.map(entities, 'originId')));
@@ -80,6 +96,7 @@ function normalizeQueriesToDatapointsByDdfql(pipe, cb) {
 
     return queryDatapointsByDdfql(pipe, subDatapointQuery, (err, pipe) => {
       console.timeEnd('get datapoints');
+
       return cb(err, pipe);
     });
   });
@@ -172,7 +189,9 @@ function getEntities(pipe, cb) {
       where: where
     };
 
-    return entitiesService.getEntities(_pipe, (err, result) => mcb(err, result.entities));
+    return entitiesService.getEntities(_pipe, (err, result) => {
+      return mcb(err, result.entities);
+    });
   }
 }
 
