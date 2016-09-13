7ac1ece2c1351dffc9a154f184ef924627e47bdd
Alex Tsap <alexandr.tsap@valor-software.com>
2016-09-13 11:38:09 +0300

    feat(ddfql): create tests for datapoints without query params

---
+++ ws.routes-tests/get-datapoints-without-query-params-second-commit.test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import test from 'ava';
+import datapointsWithoutQuery from './ws_ddf_test_fixtures_second_commit/datapoints-for-get-without-query.json'
+
+const shell = require('shelljs');
+const express = require('express');
+const git = require('simple-git');
+const api = require('supertest')('http://localhost:3000');
+
+
+function setDefaultFirstCommitByCLI(onSetDefaultByFirstCommitCLIDone) {
+  const setDefaultcommand = `REPO=git@github.com:VS-work/ddf--gapminder--systema_globalis--light.git COMMIT=dc655e1 LOGIN=dev@gapminder.org PASS=123 npm run set-default`;
+  shell.cd('../../waffle-server-import-cli');
+  return shell.exec(setDefaultcommand, (error) => {
+    console.log('** chose default set');
+
+    return onSetDefaultByFirstCommitCLIDone(error);
+
+  })
+}
+
+test.cb.before(t => {
+  console.log('Set default first commit');
+
+  return setDefaultFirstCommitByCLI(t.end);
+});
+
+test.cb('Check GET request: for datapoints without query params, when default dataset was set', t => {
+  t.plan(1);
+
+  api.get('/api/ddf/datapoints?format=json')
+    .set('Accept', 'application/x-json')
+    .expect(200)
+    .end((err, res) => {
+      t.deepEqual(res.body, datapointsWithoutQuery);
+
+      t.end();
+    })
+});
+
+test.cb('Check GET request: for datapoints without query params, when default dataset was set', t => {
+  t.plan(1);
+
+  api.get('/api/ddf/datapoints?format=wsJson')
+    .set('Accept', 'application/x-ws+json')
+    .expect(200)
+    .end((err, res) => {
+      t.deepEqual(res.body, datapointsWithoutQuery);
+
+      t.end();
+    })
+});
+
+test.cb('Check GET request: for datapoints without query params, when default dataset was set', t => {
+  t.plan(1);
+
+  api.get('/api/ddf/datapoints?format=ddfJson')
+    .set('Accept', 'application/x-ddf+json')
+    .expect(200)
+    .end((err, res) => {
+      t.deepEqual(res.body, datapointsWithoutQuery);
+
+      t.end();
+    })
+});
