0cc44ac660193d49c44b4a26f96fe2ac48441b0f
Oleksandra Kalinina <alexandra.kalinine@valor-software.com>
2018-02-05 11:19:56 +0200

    fix(GCP): fix deployment running script

--- deployment/gcp_scripts/index.ts
+++ deployment/gcp_scripts/index.ts
@@ -4,14 +4,11 @@ import { run as runUnpublish } from './autoremove';
 (async function (): Promise<void> {
   let error;
 
-  console.log('!!!!!!!!!!!!', process.env.GCP_STACK_ACTION);
-
-  error = await runDeploy();
-
-  // if (process.env.GCP_STACK_ACTION === 'unpublish') {
-  //   error = await runUnpublish();
-  // } else if (process.env.GCP_STACK_ACTION === 'deploy') {
-  // }
+  if (process.env.GCP_STACK_ACTION === 'deploy') {
+    error = await runDeploy();
+  } else if (process.env.GCP_STACK_ACTION === 'unpublish') {
+    error = await runUnpublish();
+  }
 
   if (error) {
     process.exit(1);
