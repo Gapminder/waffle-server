71ac156329c5a5c09f687dece8d3ec5519f1af61
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-02-20 18:48:49 +0200

    fixed(datapoint query normalizer): query with $in clause should be processed correctry
    Closes #594

--- ws.import/utils/concepts.utils.ts
+++ ws.import/utils/concepts.utils.ts
@@ -6,16 +6,27 @@ const TIME_CONCEPT_TYPES = new Set(constants.TIME_CONCEPT_TYPES);
 export {
   getTimeConceptGids,
   getTimeConcepts,
-  isTimeConceptType
+  isTimeConceptType,
+  getEntitySetConceptGids
 };
 
 function getTimeConceptGids(concepts: any): any[] {
   return _.map(getTimeConcepts(concepts), constants.GID);
 }
 
+function getEntitySetConceptGids(concepts: any): any[] {
+  return _.map(getEntitySetConcepts(concepts), constants.GID);
+}
+
 function getTimeConcepts(concepts: any): any[] {
   return _.chain(concepts)
     .filter((concept: any) => isTimeConceptType(_.get(concept, 'properties.concept_type')))
+    .value();
+}
+
+function getEntitySetConcepts(concepts: any): any[] {
+  return _.chain(concepts)
+    .filter((concept: any) => _.get(concept, 'properties.concept_type') === constants.CONCEPT_TYPE_ENTITY_SET)
     .value();
 }
 
