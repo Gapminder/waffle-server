5308363e54615cd46b0615736091c904a8444e9a
Sergey Khristenko <sergey.khristenko@valor-software.com>
2018-02-26 17:22:06 +0200

    test(autodeploy): added tests for common helper

--- test/autodeploy/common.helper.spec.ts
+++ test/autodeploy/common.helper.spec.ts
@@ -1,13 +1,14 @@
 import 'mocha';
 import * as sinon from 'sinon';
 import * as shell from 'shelljs';
+import { ExecOutputReturnValue } from 'shelljs';
 import { expect } from 'chai';
+import * as async from 'async';
+import { ChildProcess } from 'child_process';
 
+import { logger } from '../../ws.config/log';
 import * as commonHelpers from '../../deployment/gcp_scripts/common.helpers';
 import { constants } from '../../ws.utils/constants';
-import { ChildProcess } from 'child_process';
-import { ExecOutputReturnValue } from 'shelljs';
-import * as async from 'async';
 
 const sandbox = sinon.createSandbox();
 
@@ -60,27 +61,23 @@ describe('Common.helpers Tests', () => {
   });
 
   it('runShellCommand: exit on error', (done: Function) => {
-    const execResult = {
-      code: 1,
-      stdout: 'stdout',
-      stderr: 'stderr'
-    };
+    const shellExecResult = { code: 1, stdout: 'stdout', stderr: 'stderr' };
 
     sandbox.stub(constants, 'AUTODEPLOY_RETRY_TIMES').value(1);
     sandbox.stub(constants, 'AUTODEPLOY_RETRY_INTERVAL').value(10);
-    sandbox.stub(shell, 'exec').returns({ ...execResult });
+    sandbox.stub(shell, 'exec').returns({ ...shellExecResult });
     sandbox.stub(shell, 'error').returns(true);
 
     commonHelpers.runShellCommand('gcloud compute', {}, ((err: string, result: any) => {
-      expect(err).to.contain(execResult.code, 'Error Code');
-      expect(err).to.contain(execResult.stderr, 'StdErr');
-      expect(result).to.deep.equal(execResult);
+      expect(err).to.contain(shellExecResult.code, 'Error Code');
+      expect(err).to.contain(shellExecResult.stderr, 'StdErr');
+      expect(result).to.deep.equal(shellExecResult);
 
       done();
     }));
   });
 
-  it('runShellCommand: skip step by specific error/result', (done: Function) => {
+  it('runShellCommand: skip step on specific error', (done: Function) => {
     const skipMarkers: string[] = ['already exists', 'scaled', 'AlreadyExists', 'is already in use', 'code=404', 'was not found', 'is not a valid name'];
 
     const execStub = sandbox.stub(shell, 'exec');
@@ -89,20 +86,140 @@ describe('Common.helpers Tests', () => {
     sandbox.stub(shell, 'error').returns(false);
 
     async.eachSeries(skipMarkers, (marker: string, cb: Function) => {
-      execStub.returns({
-        code: 2,
-        stdout: 'stdout',
-        stderr: marker
-      });
+      let expectedResultFromShell = { code: 2, stdout: 'stdout', stderr: marker };
+
+      execStub.returns({ ...expectedResultFromShell });
 
       commonHelpers.runShellCommand('gcloud compute', {}, (error: string, result: ExecOutputReturnValue | ChildProcess) => {
         expect(error, 'ERROR MESSAGE').to.be.a('null');
-        expect(result).to.deep.equal({ code: 2, stdout: 'stdout', stderr: marker });
+        expect(result).to.deep.equal(expectedResultFromShell);
 
         cb();
       });
     }, (error: string) => {
       return done(error);
+    });
+  });
+
+  it('runShellCommand: skip step on specific result', (done: Function) => {
+    const skipMarkers: string[] = ['already exists', 'scaled', 'AlreadyExists', 'is already in use', 'code=404', 'was not found', 'is not a valid name'];
+
+    const execStub = sandbox.stub(shell, 'exec');
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_INTERVAL').value(10);
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_TIMES').value(2);
+    sandbox.stub(shell, 'error').returns(false);
+
+    async.eachSeries(skipMarkers, (marker: string, cb: Function) => {
+      let expectedResultFromShell = { code: 0, stdout: marker, stderr: null };
+
+      execStub.returns({ ...expectedResultFromShell });
+
+      commonHelpers.runShellCommand('gcloud compute', {}, (error: string, result: ExecOutputReturnValue | ChildProcess) => {
+        expect(error, 'ERROR MESSAGE').to.be.a('null');
+        expect(result).to.deep.equal(expectedResultFromShell);
+
+        cb();
+      });
+    }, (error: string) => {
+      return done(error);
+    });
+  });
+
+  it('runShellCommand: skip on empty stdout', (done: Function) => {
+    const expectedShellOutput = { code: 0, stdout: '', stderr: null };
+    const execStub = sandbox.stub(shell, 'exec').returns({ ...expectedShellOutput });
+
+    const loggerStub = sandbox.stub(logger, 'info');
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_INTERVAL').value(10);
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_TIMES').value(1);
+    sandbox.stub(shell, 'error').returns(false);
+
+    commonHelpers.runShellCommand('gcloud compute', {}, (error: string, result: ExecOutputReturnValue | ChildProcess) => {
+      expect(error, 'ERROR MESSAGE').to.be.a('null');
+      expect(result).to.deep.equal(expectedShellOutput);
+      sinon.assert.calledTwice(loggerStub);
+
+      done();
+    });
+  });
+
+  it('runShellCommand: skip on Docker command', (done: Function) => {
+    const expectedShellOutput = { code: 0, stdout: 'stdout', stderr: null };
+    const execStub = sandbox.stub(shell, 'exec').returns({ ...expectedShellOutput });
+
+    const loggerStub = sandbox.stub(logger, 'info');
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_INTERVAL').value(10);
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_TIMES').value(1);
+    sandbox.stub(shell, 'error').returns(false);
+
+    commonHelpers.runShellCommand('gcloud compute docker', {}, (error: string, result: ExecOutputReturnValue | ChildProcess) => {
+      expect(error, 'ERROR MESSAGE').to.be.a('null');
+      expect(result).to.deep.equal(expectedShellOutput);
+      sinon.assert.calledTwice(loggerStub);
+
+      done();
+    });
+  });
+
+  it('runShellCommand: retry if invalid JSON recieved', (done: Function) => {
+    const shellResponseStub = { code: 0, stdout: null, stderr: null };
+    const incorrectJson = 'incorrect stdout';
+    const correctJson = '{"stdout":"correct"}';
+
+    const loggerStub = sandbox.stub(logger, 'info');
+    const execStub = sandbox.stub(shell, 'exec');
+
+    execStub.onFirstCall().returns({ ...shellResponseStub, stdout: incorrectJson });
+    execStub.onSecondCall().returns({ ...shellResponseStub, stdout: correctJson });
+
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_INTERVAL').value(10);
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_TIMES').value(2);
+    sandbox.stub(shell, 'error').returns(false);
+
+    commonHelpers.runShellCommand('gcloud compute', {}, (error: string, result: ExecOutputReturnValue | ChildProcess) => {
+      expect(error, 'ERROR MESSAGE').to.be.a('null');
+      expect(result).to.deep.equal({ ...shellResponseStub, stdout: correctJson });
+      sinon.assert.calledTwice(loggerStub);
+
+      done();
+    });
+  });
+
+  it('runShellCommand: check for required data in JSON', (done: Function) => {
+    const shellResponseStub = { code: 0, stdout: null, stderr: null };
+    const correctJson = '{"stdout": {"correct": {"path":"here"} } }';
+    const options = { pathsToCheck: ['stdout.correct.path'] };
+
+    sandbox.stub(shell, 'exec').returns({ ...shellResponseStub, stdout: correctJson });
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_INTERVAL').value(10);
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_TIMES').value(1);
+    sandbox.stub(shell, 'error').returns(false);
+
+    commonHelpers.runShellCommand('gcloud compute', options, (error: string, result: ExecOutputReturnValue | ChildProcess) => {
+      expect(error, 'ERROR MESSAGE').to.be.a('null');
+      expect(result).to.deep.equal({ ...shellResponseStub, stdout: correctJson });
+
+      done();
+    });
+  });
+
+  it('runShellCommand: throw error if no required data in JSON', (done: Function) => {
+    const shellResponseStub = { code: 0, stdout: null, stderr: null };
+    const correctJson = '{"stdout": {"correct": {"NOpath":"here"} } }';
+    const optionsWithIncorrectPath = { pathsToCheck: ['stdout.correct.path'] };
+
+    sandbox.stub(shell, 'exec').returns({ ...shellResponseStub, stdout: correctJson });
+
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_INTERVAL').value(10);
+    sandbox.stub(constants, 'AUTODEPLOY_RETRY_TIMES').value(1);
+    sandbox.stub(shell, 'error').returns(false);
+
+    commonHelpers.runShellCommand('gcloud compute', optionsWithIncorrectPath, (error: string, result: ExecOutputReturnValue | ChildProcess) => {
+      expect(error).to.contain(optionsWithIncorrectPath.pathsToCheck[0]);
+      expect(error).to.contain(correctJson);
+      expect(result).to.deep.equal({ ...shellResponseStub, stdout: correctJson });
+
+      done();
     });
   });
 });
