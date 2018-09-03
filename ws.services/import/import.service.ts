bb9a28c09926f37a09a64147b15c0af866ebe3fd
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-09-03 16:39:15 +0300

    fix

--- ws.services/import/import.service.ts
+++ ws.services/import/import.service.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as threads from 'threads';
 import { isEmpty, keys } from 'lodash';
@@ -8,13 +9,25 @@ import { DataSetManager } from './dataset-manager';
 import 'rxjs/add/operator/mergeMap';
 
 const repoDescriptors = require('../../ws.config/repos.config.json');
+const importConfig = require('../../ws.config/import.config.json');
 const spawn = threads.spawn;
 
-const reposRootPath = path.resolve('.', 'ws-repos');
-const importRootPath = path.resolve('.', 'ws-import');
+const reposRootPath = path.resolve(importConfig.wsDatasetsOriginPath, 'ws-repos');
+const importRootPath = path.resolve(importConfig.wsDatasetsImportPath, 'ws-import');
 
 const importDataSet = async (repoDescriptor: RepoDescriptor) => {
   return new Promise((resolve: Function, reject: Function) => {
+    const lockFileName = `${repoDescriptor.repoNickname.replace(/\//, '-')}.lock`;
+    const lockFileNamePath = path.resolve(importRootPath, lockFileName);
+
+    const isLockFileExists = fs.pathExistsSync(lockFileNamePath);
+
+    if (isLockFileExists) {
+      return resolve();
+    }
+
+    fs.writeFileSync(lockFileNamePath, new Date().toUTCString());
+
     const thread = spawn((input: any, done: Function) => done({
       repoDescriptor: input.repoDescriptor
     }));
@@ -37,6 +50,7 @@ const importDataSet = async (repoDescriptor: RepoDescriptor) => {
           logger.error(flowErr);
         } finally {
           thread.kill();
+          fs.removeSync(lockFileNamePath);
         }
       })
       .on('error', (error: any) => reject(error))
